4e93096d96297fca5a25d8a3715a081d15e16dcc
Lillian Zhang <Lillian Zhang>
2018-04-20 02:18:01 -0400

    created safe buffer for querying

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "academic-mysql.cc.gatech.edu",
   user: "cs4400_team_27",
-  password: "FSuJjLjh"
+  password: "FSuJjLjh",
+  database: "cs4400_team_27"
 });
-sql = "SELECT * FROM User";
-con.connect(function(err) {
+con.connect();
+
+sql = "INSERT INTO User (Username,Email,Password,UserType) VALUES ('lillian', 'lillianzhang@gatech.edu', 'd68fae04506bde7857ff4aa40ebad49c', 'ADMIN')";
+con.query(sql, function (err, result, fields) {
   if (err) throw err;
-  console.log("Connected!");
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
+  console.log(JSON.stringify(result));
 });
+con.end();
