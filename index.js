02e8e6d5ea32c3331658c52c502cca5f3fcda6e9
Dennis Eddington <Dedding4341@gmail.com>
2018-04-20 04:00:06 -0400

    Housekeeping

--- index.js
+++ index.js
@@ -6,20 +6,13 @@ var handlebars = require('handlebars');
 
 
 // var creds = require('credentials.js');
-var con = mysql.createConnection({
+var connection = mysql.createConnection({
   host: "academic-mysql.cc.gatech.edu",
   user: "cs4400_team_27",
   password: "FSuJjLjh",
   database: "cs4400_team_27"
 });
-con.connect();
-
-sql = "INSERT INTO User (Username,Email,Password,UserType) VALUES ('lillian', 'lillianzhang@gatech.edu', 'd68fae04506bde7857ff4aa40ebad49c', 'ADMIN')";
-con.query(sql, function (err, result, fields) {
-  if (err) throw err;
-  console.log(JSON.stringify(result));
-});
-con.end();
+connection.connect();
 
 
 var app = express();
